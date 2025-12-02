d05318a9d3a8f8f31faa2eddeb223e530854efc8
nahom <tnguyen@ntis.gov>
2025-12-01 23:14:45 -0500

    new-9

--- homework/train_planner.py
+++ homework/train_planner.py
@@ -15,54 +15,7 @@ from torch.utils.data import DataLoader
 from homework.datasets.road_dataset import RoadDataset
 from models import MLPPlanner, TransformerPlanner, CNNPlanner, save_model
 
-from homework.datasets.road_dataset import RoadDataset
-from homework.datasets.road_transforms import Compose, ImageLoader, EgoTrackProcessor
-
-
-from homework.datasets.road_utils import Track
-import glob
-
-def load_planner_dataset(dataset_path, batch_size, shuffle):
-    # dataset_path points to "drive_data/train" or "drive_data/val"
-    episode_paths = sorted(glob.glob(os.path.join(dataset_path, "*")))
-
-    datasets = []
-    for episode_path in episode_paths:
-        track = Track(os.path.join(episode_path, "track.npz"))
-
-        # Compose a planner-friendly transform
-        transform = Compose([
-            EgoTrackProcessor(track, n_track=10, n_waypoints=3),
-        ])
-
-        ds = RoadDataset(episode_path, transform_pipeline=None)
-        ds.transform = transform  # Manually override the transform!
-        datasets.append(ds)
-
-    from torch.utils.data import ConcatDataset
-    full = ConcatDataset(datasets)
-
-    return DataLoader(full, batch_size=batch_size, shuffle=shuffle)
-
-
-def load_cnn_dataset(dataset_path, batch_size, shuffle):
-    episode_paths = sorted(glob.glob(os.path.join(dataset_path, "*")))
-
-    datasets = []
-    for episode_path in episode_paths:
-        transform = Compose([
-            ImageLoader(episode_path),
-        ])
-        ds = RoadDataset(episode_path, transform_pipeline=None)
-        ds.transform = transform
-        datasets.append(ds)
-
-    from torch.utils.data import ConcatDataset
-    full = ConcatDataset(datasets)
-
-    return DataLoader(full, batch_size=batch_size, shuffle=shuffle)
-
-
+from homework.datasets.road_dataset import load_data
 
 
 
@@ -116,31 +69,19 @@ def main():
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
     # Load data
-    if args.model in ["mlp_planner", "transformer_planner"]:
-        train_loader = load_planner_dataset(
-            "../drive_data/train",
-            args.batch_size,
-            shuffle=True,
-        )
-        val_loader = load_planner_dataset(
-            "../drive_data/val",
-            args.batch_size,
-            shuffle=False,
-        )
+    train_loader = load_data(
+        dataset_path="../drive_data/train",
+        transform_pipeline="default",
+        batch_size=args.batch_size,
+        shuffle=True,
+    )
 
-    elif args.model == "cnn_planner":
-        train_loader = load_cnn_dataset(
-            "../drive_data/train",
-            args.batch_size,
-            shuffle=True,
-        )
-        val_loader = load_cnn_dataset(
-            "../drive_data/val",
-            args.batch_size,
-            shuffle=False,
-        )
-    else:
-        raise ValueError(f"Unknown model {args.model}")
+    val_loader = load_data(
+        dataset_path="../drive_data/val",
+        transform_pipeline="default",
+        batch_size=args.batch_size,
+        shuffle=False,
+    )
 
     # Model setup
     model = get_model(args.model).to(device)
